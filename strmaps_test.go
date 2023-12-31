774367dffcab5702ead442f122a6762014cb2ce6
Ryan Ogden <12yanogden@gmail.com>
2023-12-31 10:07:25 -0500

    move most func()s to generic maps package

--- strmaps_test.go
+++ strmaps_test.go
@@ -1,59 +1,14 @@
 package strmaps
 
 import (
-	"strconv"
 	"testing"
 )
 
-func TestMapEquality(t *testing.T) {
-	m1 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
-	m2 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
-	expected := true
-	actual := Equals(m1, m2)
-
-	if !actual {
-		t.Fatalf("\nExpected: '" + strconv.FormatBool(expected) + "', they are equal\nActual: '" + strconv.FormatBool(actual) + "', they are not equal")
-	}
-}
-
-func TestMapInequalityByLength(t *testing.T) {
-	m1 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
-	m2 := map[string]string{"key1": "value1", "key2": "value2"}
-	expected := false
-	actual := Equals(m1, m2)
-
-	if actual {
-		t.Fatalf("\nExpected: '" + strconv.FormatBool(expected) + "', they are inequal\nActual: '" + strconv.FormatBool(actual) + "', they are equal")
-	}
-}
-
-func TestMapInequalityByKeys(t *testing.T) {
-	m1 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
-	m2 := map[string]string{"key1": "value1", "key2": "value2", "key": "value3"}
-	expected := false
-	actual := Equals(m1, m2)
-
-	if actual {
-		t.Fatalf("\nExpected: '" + strconv.FormatBool(expected) + "', they are inequal\nActual: '" + strconv.FormatBool(actual) + "', they are equal")
-	}
-}
-
-func TestMapInequalityByValues(t *testing.T) {
-	m1 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
-	m2 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value"}
-	expected := false
-	actual := Equals(m1, m2)
-
-	if actual {
-		t.Fatalf("\nExpected: '" + strconv.FormatBool(expected) + "', they are inequal\nActual: '" + strconv.FormatBool(actual) + "', they are equal")
-	}
-}
-
 // Convert string map to string
-func TestMapToString(t *testing.T) {
+func TestPretty(t *testing.T) {
 	mapp := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
 	expected := "key1: value1\nkey2: value2\nkey3: value3\n"
-	actual := ToString(mapp)
+	actual := Pretty(mapp)
 
 	if expected != actual {
 		t.Fatalf("\nExpected: '" + expected + "'\nActual: '" + actual + "'")
